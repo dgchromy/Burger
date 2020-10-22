3367e20f9a9a1d1b6cf8e8a8ec425195a57bef6e
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-10-22 14:52:42 -0700

    added connection to orm.js

---
+++ config/orm.js
@@ -0,0 +1,37 @@
+var connection = require("./connetion.js");
+
+var orm = {
+    selectAll: function (tableName, cb){
+        var queryString = "SELECT * FROM ??"
+
+        connection.query(queryString, [tableName], function (err, result){
+            if (err) throw err; 
+
+            cb(result);
+        });
+
+    },
+
+    insertOne: function (tableName, newDataObject, cb) {
+        var queryString = "INSERT INTO ?? SET ?" ;
+        connection.query(queryString, [tableName, newDataObject], function (err, result){ 
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+
+        });
+    },
+
+    updateOne: function (tableName, updatedDataObject, id, cb){
+       var queryString = "UPDATE ?? SET ? WHERE id = ?"
+       connection.query(queryString, [tableName, updateDataObject, id], function (err, result){
+           if (err) throw err;
+           console.log(result);
+           cb(result);
+       });
+    }
+
+};
+
+module.exports = orm; 
+
