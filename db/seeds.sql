50a29a36304f7c97dfd1b824063550ce68892fd6
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-10-22 14:25:25 -0700

    added burger seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) values ("Double double burger");
+INSERT INTO burgers (burger_name, devoured) values (" Black and blue burger");
+INSERT INTO burgers (burger_name, devoured) values ("Chicken bacon burger");
